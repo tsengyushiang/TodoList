25a3bfba6bd063b1010a998debc7c37d38700da2
yushiang.tseng <yuhsinag.tseng@kdanmobile.com>
2022-10-27 08:41:14 +0800

    add id to mockup todos

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -3,22 +3,27 @@ export const todos = [
   {
     text: "新增Todo項目",
     tagIds: ["open"],
+    id: "#0",
   },
   {
     text: "刪除Todo項目",
     tagIds: ["open"],
+    id: "#1",
   },
   {
     text: "- 選擇項目\n- 更改項目",
     tagIds: ["close"],
+    id: "#2",
   },
   {
     text: "篩選Todo項目",
     tagIds: ["ready"],
+    id: "#3",
   },
   {
     text: "查詢Todo項目",
     tagIds: ["running"],
+    id: "#4",
   },
 ];
 
